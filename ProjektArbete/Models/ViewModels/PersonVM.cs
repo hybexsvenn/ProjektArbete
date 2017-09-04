4f8aff5a6d9cbc3060b025c9052edf50570393b1
Veronica Roeoes <roos.veronica@gmail.com>
2017-09-04 11:24:57 +0200

    ViewModel Person

---
+++ ProjektArbete/Models/ViewModels/PersonVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektArbete.Models.ViewModels
+{
+    public enum Vote
+    {
+        Ja,
+        Nej, 
+        Avstår,
+        Frånvarande
+    }
+
+    public class PersonVM
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Party { get; set; }
+        public string Constituency { get; set; }
+        public string ConstituencyNumber { get; set; }
+        public Vote Vote { get; set; }
+        public string ParliamentaryYear { get; set; }
+        public string Status { get; set; }
+
+    }
+}
