625ebf12202a5bca7a20d510439a242d543e5184
Mikael Svenn <hybexsvenn@gmail.com>
2017-09-04 12:47:13 +0200

    Lite nytt layout

---
+++ ProjektArbete/Models/Valkrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektArbete.Models
+{
+    public class Valkrets
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ ProjektArbete/Models/ViewModels/WrapperVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektArbete.Models.ViewModels
+{
+    public class WrapperVM
+    {
+        public Valkrets[] valkrets { get; set; }
+    }
+}
