127984d977053ca754b06fa35b8c5292f64b7e2e
Veronica Roeoes <roos.veronica@gmail.com>
2017-09-04 15:13:16 +0200

    Fixes

---
+++ ProjektArbete/Models/ViewModels/PartyVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjektArbete.Models.ViewModels
+{
+    public class PartyVM
+    {
+        public Party Party { get; set; }
+        public PersonVM[] Members { get; set; }
+        public Vote[] Vote { get; set; }
+        public string Constituency { get; set; }
+        public string ConstituencyNumber { get; set; }
+        public string ParliamentaryYear { get; set; }
+        public string Status { get; set; }
+    }
+}
