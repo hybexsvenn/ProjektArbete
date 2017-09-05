58c7ced943710bb1ecd2f7bcb4cc713bff1cf3eb
Pontus Lind <Pontus.lindh.92@gmail.com>
2017-09-05 15:08:05 +0200

    Slider

--- ProjektArbete/wwwroot/myScript.js
+++ ProjektArbete/wwwroot/myScript.js
@@ -14,8 +14,30 @@ $(function () {
     });
 
     $("#date").val($("#slider-range").slider("values", 0) +
-    $("#slider-range").slider("values", 1));
+        $("#slider-range").slider("values", 1));
+
+    $("#slider-range").mousedown(function () {
+        $("#date").append("<span style='color:#00f;'>Mouse down.</span>");
+    });
+
 });
+
+$(document).ready(function () {
+    var temp = false;
+    $("#slider-range").mousedown(function () {
+        temp = true;
+    });
+    
+        $("body").mouseup(function () {
+            if (temp === true) {
+            alert("Yay:D");
+            temp = false;
+            }
+        });
+    
+});
+        
+
 
 
 
