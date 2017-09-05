f89ec585881ae5d919082d11744009ade1fc364b
Pontus Lind <Pontus.lindh.92@gmail.com>
2017-09-05 13:35:23 +0200

    yay

--- ProjektArbete/wwwroot/myScript.js
+++ ProjektArbete/wwwroot/myScript.js
@@ -22,7 +22,22 @@ app.controller("testController", function ($scope) {
     };
 });
 
+// Range slider
 
+$(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 2003,
+        max: 2017,
+        values: [2014, 2017],
+        slide: function (event, ui) {
+            $("#date").val(ui.values[0] + " - " + ui.values[1]);
+        }
+    });
+
+    $("#date").val($("#slider-range").slider("values", 0) +
+    $("#slider-range").slider("values", 1));
+});
 
 
 
