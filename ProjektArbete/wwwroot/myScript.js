1927aa04c06af8e25fac72d36003dd59b9f93eb5
Veronica Roeoes <roos.veronica@gmail.com>
2017-09-07 18:45:23 +0200

    Navbar

--- ProjektArbete/wwwroot/myScript.js
+++ ProjektArbete/wwwroot/myScript.js
@@ -2,7 +2,24 @@
     $('.partyLogo').hover(function () {
         $('.partyLogo').not(this).toggleClass('opacityLogos');
     });
+
+    fixNavBar();
 });
+
+function fixNavBar() {
+   
+    $(".navbar-nav").children('li').each(function () {
+
+        var myHref = $(this).children('a').first().attr('href');
+
+        if (window.location.href.endsWith(myHref)) {
+            $(this).addClass('active');
+        }
+        else {
+            $(this).removeClass('active');
+        }
+    });
+}
 
 // Range slider
 var dataForIndex;
