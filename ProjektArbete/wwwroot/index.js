6f3887832c4ccc972770a0b7829b0fb55d3918c6
Pontus Lind <Pontus.lindh.92@gmail.com>
2017-09-08 15:08:27 +0200

    Index klar

--- ProjektArbete/wwwroot/index.js
+++ ProjektArbete/wwwroot/index.js
@@ -1,79 +1,149 @@
-﻿
-
+﻿var r = [];
+var sliderStart;
+var sliderEnd;
+var listOfPartbetweenStartAndEndYear = [];
+var listofPartForX = [];
 
 $(document).ready(function () {
     $.ajax({
         url: "/Api/Index",
         type: 'GET',
         dataType: 'json',
-        success: function (r) {
-            console.log(r);
-            var V = [];
-            for (var i = 0; i < r.length; i++) {
-                if (r[i].party === "-") {
+        success: function (re) {
+            for (var i = 0; i < re.length; i++) {
+                if (re[i].party === "-") {
 
-                } else if (r[i].party === "V") {
-                    V.push(r[i].percentageAbsence);
+                }
+                else {
+                    re[i].year = re[i].year.substring(0, re[i].year.length - 3)
+                    r.push(re[i]);
                 }
             }
-            var VP = 0;
-            for (var i = 0; i < V.length; i++) {
-                VP += V[i];
-            }
-            var VPP = VP / V.length;
-
-            console.log(V.length);
-            console.log(VP);
-            console.log(VPP);
-            
-
-
-
-
-
-
-            var ctx = document.getElementById("myChart").getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: [r[0].party, r[1].party, r[2].party, r[3].party, r[4].party, r[5].party, r[6].party, r[7].party],
-                    datasets: [{
-                        label: 'Närvaro',
-                        data: [r[0].percentageAbsence, r[1].percentageAbsence, r[2].percentageAbsence, r[3].percentageAbsence, r[4].percentageAbsence, r[5].percentageAbsence, r[6].percentageAbsence, r[7].percentageAbsence],
-                        backgroundColor: [
-                            'rgba(237, 28, 36, 0.8)',
-                            'rgba(237, 27, 52, 0.8)',
-                            'rgba(83, 160, 69, 0.8)',
-                            'rgba(1, 106, 58, 0.8)',
-                            'rgba(0, 106, 179, 0.8)',
-                            'rgba(0, 93, 160, 0.8)',
-                            'rgba(13, 157, 219, 0.8)',
-                            'rgba(37, 34, 85, 0.8)'
-                        ],
-                        borderColor: [
-                            'rgba(237, 28, 36, 1)',
-                            'rgba(237, 27, 52, 1)',
-                            'rgba(83, 160, 69, 1)',
-                            'rgba(1, 106, 58, 1)',
-                            'rgba(0, 106, 179, 1)',
-                            'rgba(0, 93, 160, 1)',
-                            'rgba(13, 157, 219, 1)',
-                            'rgba(37, 34, 85, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
+            ByYear("2014", "2017");
+            GetProcentByParty(listOfPartbetweenStartAndEndYear);
+            GenerateChartIndex(listofPartForX);
+        }
+    });
+    $("#date").val("2014/15 - 2016/17");
+    var temp = false;
+    $("#slider-range").mousedown(function () {
+        temp = true;
+    });
+    $("body").mouseup(function () {
+        if (temp === true) {
+            ByYear(sliderStart, sliderEnd);
+            GetProcentByParty(listOfPartbetweenStartAndEndYear);
+            GenerateChartIndex(listofPartForX);
+            temp = false;
         }
     });
 });
 
+function ByYear(start, end) {
+    for (var i = 0; i < r.length; i++) {
+        if (r[i].year >= start && r[i].year <= end) {
+            listOfPartbetweenStartAndEndYear.push({ party: r[i].party, percentageAbsence: r[i].percentageAbsence })
+        }
+    }
+}
+
+function functiontofindIndexByKeyValue(arraytosearch, key, valuetosearch) {
+
+    for (var i = 0; i < arraytosearch.length; i++) {
+
+        if (arraytosearch[i][key] == valuetosearch) {
+            return i;
+        }
+    }
+    return null;
+}
+
+function GetProcentByParty(ret) {
+
+    for (var i = 0; i < ret.length; i++) {
+        if (ifExist(ret[i].party)) {
+            var a = functiontofindIndexByKeyValue(listofPartForX, "party", ret[i].party);
+            var t = listofPartForX[a].partyA;
+            listofPartForX[a].partyA = ((t + ret[i].percentageAbsence) / 2)
+        }
+        else {
+            listofPartForX.push({ party: ret[i].party, partyA: ret[i].percentageAbsence });
+        }
+    }
+}
+function ifExist(inValu) {
+    if (listofPartForX.length === 0) {
+        return false;
+    }
+    else {
+        for (var i = 0; i < listofPartForX.length; i++) {
+            if (inValu === listofPartForX[i].party) { return true; }
+        }
+        return false;
+    }
+}
+
+$(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 2003,
+        max: 2017,
+        values: [2013, 2015],
+        slide: function (event, ui) {
+            $("#date").val(ui.values[0] + "/" + ((ui.values[0] % 100) + 1) + " - " + ui.values[1] + "/" + ((ui.values[1] % 100) + 1));         
+            sliderStart = ui.values[0];
+            sliderEnd = ui.values[1];
+        }
+    });
+
+    $("#slider-range").mousedown(function () {
+        $("#date").append("<span style='color:#00f;'>Mouse down.</span>");
+    });
+});
+
+function GenerateChartIndex(indata) {
+     $('#myChart').remove();
+     $('#indexDivCanvas').append(' <canvas id="myChart" width="400" height="200"></canvas>');
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [indata[0].party, indata[1].party, indata[2].party, indata[3].party, indata[4].party, indata[5].party, indata[6].party, indata[7].party],
+            datasets: [{
+                label: 'Närvaro',
+                data: [100 - indata[0].partyA, 100 - indata[1].partyA, 100 - indata[2].partyA, 100 - indata[3].partyA, 100 - indata[4].partyA, 100 - indata[5].partyA, 100 - indata[6].partyA, 100 - indata[7].partyA],
+                backgroundColor: [
+                    'rgba(237, 28, 36, 0.8)',
+                    'rgba(237, 27, 52, 0.8)',
+                    'rgba(83, 160, 69, 0.8)',
+                    'rgba(1, 106, 58, 0.8)',
+                    'rgba(0, 106, 179, 0.8)',
+                    'rgba(0, 93, 160, 0.8)',
+                    'rgba(13, 157, 219, 0.8)',
+                    'rgba(37, 34, 85, 0.8)'
+                ],
+                borderColor: [
+                    'rgba(237, 28, 36, 1)',
+                    'rgba(237, 27, 52, 1)',
+                    'rgba(83, 160, 69, 1)',
+                    'rgba(1, 106, 58, 1)',
+                    'rgba(0, 106, 179, 1)',
+                    'rgba(0, 93, 160, 1)',
+                    'rgba(13, 157, 219, 1)',
+                    'rgba(37, 34, 85, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+  
--- ProjektArbete/wwwroot/myScript.js
+++ ProjektArbete/wwwroot/myScript.js
@@ -23,101 +23,101 @@ function fixNavBar() {
 // Range slider
 var dataForIndex;
 
-$(function () {
+//$(function () {
 
-    $("#slider-range").slider({
-        range: true,
-        min: 2003,
-        max: 2017,
-        values: [2014, 2017],
-        slide: function (event, ui) {
-            $("#date").val(ui.values[0] + " - " + ui.values[1]);
-            dataForIndex  = ui.values[0] + ";" + ui.values[1];
-        }
-    });
+//    $("#slider-range").slider({
+//        range: true,
+//        min: 2003,
+//        max: 2017,
+//        values: [2014, 2017],
+//        slide: function (event, ui) {
+//            $("#date").val(ui.values[0] + " - " + ui.values[1]);
+//            dataForIndex  = ui.values[0] + ";" + ui.values[1];
+//        }
+//    });
 
-    $("#date").val($("#slider-range").slider("values", 0) +
-        $("#slider-range").slider("values", 1));
+//    $("#date").val($("#slider-range").slider("values", 0) +
+//        $("#slider-range").slider("values", 1));
 
-    $("#slider-range").mousedown(function () {
-        $("#date").append("<span style='color:#00f;'>Mouse down.</span>");
-    });
+//    $("#slider-range").mousedown(function () {
+//        $("#date").append("<span style='color:#00f;'>Mouse down.</span>");
+//    });
 
-    //$('li a').click(function (e) {
-    //    var $this = $(this);
-    //    if (!$this.hasClass('active')) {
-    //        $this.addClass('active');
-    //    }
-    //    e.preventDefault();
-    //});
+//    //$('li a').click(function (e) {
+//    //    var $this = $(this);
+//    //    if (!$this.hasClass('active')) {
+//    //        $this.addClass('active');
+//    //    }
+//    //    e.preventDefault();
+//    //});
 
-});
+//});
 
-$(document).ready(function () {
-    $("#date").val("2014 - 2017");
-    var temp = false;
-    $("#slider-range").mousedown(function () {
-        temp = true;
-    });
+//$(document).ready(function () {
+//    $("#date").val("2014 - 2017");
+//    var temp = false;
+//    $("#slider-range").mousedown(function () {
+//        temp = true;
+//    });
 
-        $("body").mouseup(function () {
-            if (temp === true) {
-                $.ajax({
-                    url: "/Api/Index/" + dataForIndex,
-                    type: 'GET',
-                    dataType: 'json',
-                    success: function (r) {
-                        console.log(r);
-                        $('#myChart').remove();
-                        $('#indexDivCanvas').append(' <canvas id="myChart" width="400" height="200"></canvas>');
-                        var ctx = document.getElementById("myChart").getContext('2d');
-                        var myChart = new Chart(ctx, {
-                            type: 'bar',
-                            data: {
-                                labels: [r[0].party, r[1].party, r[2].party, r[3].party, r[4].party, r[5].party, r[6].party, r[7].party],
-                                datasets: [{
-                                    label: 'Närvaro',
-                                    data: [r[0].percentageAbsence, r[1].percentageAbsence, r[2].percentageAbsence, r[3].percentageAbsence, r[4].percentageAbsence, r[5].percentageAbsence, r[6].percentageAbsence, r[7].percentageAbsence],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.8)',
-                                        'rgba(54, 162, 235, 0.8)',
-                                        'rgba(255, 206, 86, 0.8)',
-                                        'rgba(75, 192, 192, 0.8)',
-                                        'rgba(153, 102, 255, 0.8)',
-                                        'rgba(153, 102, 255, 0.8)',
-                                        'rgba(153, 102, 255, 0.8)',
-                                        'rgba(255, 159, 64, 0.8)'
-                                    ],
-                                    borderColor: [
-                                        'rgba(255,99,132,1)',
-                                        'rgba(54, 162, 235, 1)',
-                                        'rgba(255, 206, 86, 1)',
-                                        'rgba(75, 192, 192, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(255, 159, 64, 1)'
-                                    ],
-                                    borderWidth: 1
-                                }]
-                            },
-                            options: {
-                                scales: {
-                                    yAxes: [{
-                                        ticks: {
-                                            beginAtZero: true
-                                        }
-                                    }]
-                                }
-                            }
-                        });
-                    }
-                });
+//        $("body").mouseup(function () {
+//            if (temp === true) {
+//                $.ajax({
+//                    url: "/Api/Index/" + dataForIndex,
+//                    type: 'GET',
+//                    dataType: 'json',
+//                    success: function (r) {
+//                        console.log(r);
+//                        $('#myChart').remove();
+//                        $('#indexDivCanvas').append(' <canvas id="myChart" width="400" height="200"></canvas>');
+//                        var ctx = document.getElementById("myChart").getContext('2d');
+//                        var myChart = new Chart(ctx, {
+//                            type: 'bar',
+//                            data: {
+//                                labels: [r[0].party, r[1].party, r[2].party, r[3].party, r[4].party, r[5].party, r[6].party, r[7].party],
+//                                datasets: [{
+//                                    label: 'Närvaro',
+//                                    data: [r[0].percentageAbsence, r[1].percentageAbsence, r[2].percentageAbsence, r[3].percentageAbsence, r[4].percentageAbsence, r[5].percentageAbsence, r[6].percentageAbsence, r[7].percentageAbsence],
+//                                    backgroundColor: [
+//                                        'rgba(255, 99, 132, 0.8)',
+//                                        'rgba(54, 162, 235, 0.8)',
+//                                        'rgba(255, 206, 86, 0.8)',
+//                                        'rgba(75, 192, 192, 0.8)',
+//                                        'rgba(153, 102, 255, 0.8)',
+//                                        'rgba(153, 102, 255, 0.8)',
+//                                        'rgba(153, 102, 255, 0.8)',
+//                                        'rgba(255, 159, 64, 0.8)'
+//                                    ],
+//                                    borderColor: [
+//                                        'rgba(255,99,132,1)',
+//                                        'rgba(54, 162, 235, 1)',
+//                                        'rgba(255, 206, 86, 1)',
+//                                        'rgba(75, 192, 192, 1)',
+//                                        'rgba(153, 102, 255, 1)',
+//                                        'rgba(153, 102, 255, 1)',
+//                                        'rgba(153, 102, 255, 1)',
+//                                        'rgba(255, 159, 64, 1)'
+//                                    ],
+//                                    borderWidth: 1
+//                                }]
+//                            },
+//                            options: {
+//                                scales: {
+//                                    yAxes: [{
+//                                        ticks: {
+//                                            beginAtZero: true
+//                                        }
+//                                    }]
+//                                }
+//                            }
+//                        });
+//                    }
+//                });
 
-            temp = false;
-            }
-        });
-});
+//            temp = false;
+//            }
+//        });
+//});
         
 
 
